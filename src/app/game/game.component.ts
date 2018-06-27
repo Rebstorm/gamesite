b041deea25d4f85a2e2828cfa80496636a0fb5e5
Paul Jacks <paul.jacks@scopevisio.com>
2018-06-27 15:41:37 +0200

    NOW HE JUMP SOOOPER FAAAST

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -37,6 +37,7 @@ export class GameComponent implements AfterViewInit {
     // Load the stuff for the game.
     PIXI.loader
       .add("guy-atlas", "../../assets/img/sprites/char.json" )
+      .add("guy-atlas-falling", "../../assets/img/sprites/char_falling.json")
       .add("floor", "../../assets/img/ground_2.png")
       .load(() => {
         this.createGameAssets();
@@ -47,13 +48,11 @@ export class GameComponent implements AfterViewInit {
 
   createGameAssets(): any {    
     // Get our running guy.
-    this.gameGuy = new RunningGuy(this.canvasHeight, this.canvasWidth);
+    this.gameGuy = new RunningGuy(this.canvasHeight, this.canvasWidth, this.pixiApp);
 
     // Get our ground.
     this.floor = new Floor(this.canvasHeight, this.canvasWidth);
-    
-
-  
+      
     /*  
     * Add the stuff to the scene.
     * 
@@ -65,15 +64,65 @@ export class GameComponent implements AfterViewInit {
     // BG
     this.pixiApp.stage.addChild(this.floor);
 
-   
+    this.createEventHandlers();
+
+    this.createCollisionDetection();
 
 
     this.startGame();
   }
 
+  createEventHandlers(){
+
+    document.addEventListener('keydown', e => {
+      
+      if(this.gameGuy.isJumping)
+        return;
+
+      if(e.keyCode == 38 ){ // the up arrow
+        this.gameGuy.isJumping = true;
+        // Adding the jumping speed.
+        this.gameGuy.addJumpSpeed(-15);
+
+        this.pixiApp.ticker.addOnce( (e)=> { this.checkIfOnFloor(e) };
+        
+      }
+    
+    });
+  }
+
+  checkIfOnFloor(e){
+    window.setTimeout(()=> {
+      if(this.objectsColliding(this.gameGuy, this.floor))
+        this.gameGuy.isJumping = false;
+      else
+        this.checkIfOnFloor(e);
+
+    }, 250)
+  }
+
   startGame(){
     this.floor.startGround(this.pixiApp);
 
+  }
+
+  createCollisionDetection(){
+    // Creating collision between guy and floor. 
+    this.pixiApp.ticker.add((delta) => {
+        //console.log(this.objectsColliding(this.gameGuy, this.floor));
+        if(this.objectsColliding(this.gameGuy, this.floor) && !this.gameGuy.isJumping){
+          this.gameGuy.jumpingSpeedY = 0;
+          this.gameGuy.position.y = this.canvasHeight / 1.25;
+        }
+    });
+
+  }
+
+  objectsColliding(a:PIXI.Sprite, b: PIXI.Sprite): boolean{
+    let ab = a.getBounds();
+    let bb = b.getBounds();
+
+    return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
   }
 
   
@@ -87,12 +136,14 @@ class RunningGuy extends PIXI.Sprite {
 
   private GRAVITY = 9.8;
   private GAME_SPEED_X = 40;
-  public runningSpeedY: number = 0;
+  public jumpingSpeedY: number = 0;
+  public isJumping = false;
+  private pixiApp: PIXI.Application;
   
 
-  constructor(canvasHeight: number, canvasWidth: number) {
+  constructor(canvasHeight: number, canvasWidth: number, pixiApp: PIXI.Application) {
     super();
-
+    this.pixiApp = pixiApp;
     this.canvasHeight = canvasHeight;
     this.canvasWidth = canvasWidth;
 
@@ -105,37 +156,42 @@ class RunningGuy extends PIXI.Sprite {
     // Offset for ground and stuff.
     this.y = canvasHeight / 1.25;
 
-
     this.scale.x *= 1.50;
     this.scale.y *= 1.50;
 
     // Todo: find out what interactive means. But it sounds fancy. 
-    this.interactive = true;
-
-    
-    document.addEventListener('keydown', e => {
-      this.addSpeed(-this.GRAVITY / 3);
-      console.log("keydown pressed");
-    });
 
     document.addEventListener("keyup", (e) => {
 
     });
 
     // We got to animate the dude, yo. 
-    window.setInterval(()=> { this.animateGuy() }, 50);
+    pixiApp.ticker.add(()=> { this.animateGuy() });
 
     
   }
 
-  private guyTextureCounter: number = 0;
+  private guyRunningTextureCounter: number = 0;
+  private guyJumpingTextureCounter: number = 0;
   animateGuy() {
-      this.guyTextureCounter++;
-      if(this.guyTextureCounter < 8){
-        this.texture = PIXI.loader.resources["guy-atlas"].textures["frame_" + this.guyTextureCounter + ".gif"];
+
+      if(this.isJumping){
+        if(this.guyJumpingTextureCounter < 2){
+          this.texture = PIXI.loader.resources["guy-atlas-falling"].textures["falling_" + this.guyJumpingTextureCounter + ".png"];
+          this.guyJumpingTextureCounter++;
+        } else {
+          this.texture = PIXI.loader.resources["guy-atlas-falling"].textures["falling_0.png"];
+          this.guyJumpingTextureCounter = 0;
+        }
+  
       } else {
-        this.guyTextureCounter = 0;
-        this.texture = PIXI.loader.resources["guy-atlas"].textures["frame_0.gif"];
+        if(this.guyRunningTextureCounter < 8){
+          this.texture = PIXI.loader.resources["guy-atlas"].textures["frame_" + this.guyRunningTextureCounter + ".gif"];
+          this.guyRunningTextureCounter++;
+        } else {
+          this.guyRunningTextureCounter = 0;
+          this.texture = PIXI.loader.resources["guy-atlas"].textures["frame_0.gif"];
+        }
       }
 
       this.updateSprite();
@@ -143,25 +199,19 @@ class RunningGuy extends PIXI.Sprite {
 
   
 
-  addSpeed(speedInc: number) {
-    this.runningSpeedY += speedInc;
-    this.runningSpeedY = Math.max(-this.GRAVITY, this.runningSpeedY);
+  addJumpSpeed(speedInc: number) {
+    this.jumpingSpeedY += speedInc;
+    //this.jumpingSpeedY = Math.max(-this.GRAVITY, this.jumpingSpeedY);
+    //console.log(Math.max(-this.GRAVITY, this.jumpingSpeedY));
   }
   
 
-  updateSprite(){
-    /*
-    this.speedY += this.GRAVITY / 70;
+  updateSprite(){   
+    this.jumpingSpeedY += this.GRAVITY / this.pixiApp.ticker.elapsedMS * 2 ;
     
     // todo: boolean grounded. 
-    if(this.y < this.canvasHeight / 1.15){
-      this.speedY = 0; 
-      return;
-    } else {   
-      this.y += this.speedY;
-    }
-
-    */
+    this.y += this.jumpingSpeedY;
+    
     
     //this.rotation = Math.atan(this.speedY / this.GAME_SPEED_X);
 
@@ -194,9 +244,6 @@ class Floor extends PIXI.extras.TilingSprite {
     // Offset for ground and stuff.
     this.y = canvasHeight / 1.15;
 
-    //this.scale.x = 1;
-    //this.scale.y = 0.8;
-    
     this.tileScale.y = 0.8;
     this.tileScale.x = 0.8;
     this.tilePosition.x = 0;
@@ -209,9 +256,8 @@ class Floor extends PIXI.extras.TilingSprite {
   }
 
   private move(delta:number){
-    //this.x -= 1 + delta;
-    console.log(this.x);
-
+    
+    // moving the tileposition with tileposition x minus delta, modulus texture width times the scale of the texture. 
     this.tilePosition.x = (this.tilePosition.x - delta) % ( this.texture.width * 0.8 );
 
   }
